00d6e966894fa21bb6827e4bf1c6d058a8506332
Mehdi Essemlali El Attar <mehdiessemlali00@gmail.com>
2026-05-01 21:09:25 +0100

    Initial commit: Add backend and frontend

---
+++ backend-spring/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,57 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUser;
+    private String login;
+    private String password;
+    private Integer connectionNumber;
+
+    public User() {
+    }
+
+    public User(String login, String password, Integer connectionNumber) {
+        this.login = login;
+        this.password = password;
+        this.connectionNumber = connectionNumber;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Integer getConnectionNumber() {
+        return connectionNumber;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setConnectionNumber(Integer connectionNumber) {
+        this.connectionNumber = connectionNumber;
+    }
+}
